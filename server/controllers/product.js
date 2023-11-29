8eeeda40afceda3edc48ab45c7a7265734f1280a
duc vinh <ducvinh100503@gmail.com>
2023-11-29 22:53:03 +0700

    ratings

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -33,8 +33,6 @@ const getAllProduct = asyncHandler(async(req, res) => {
     queryString = queryString.replace(/\b(gte|gt|lt|lte)\b/g, el => `$${el}`)
     const formatedQueries = JSON.parse(queryString)
 
-    console.log(formatedQueries)
-
     // Filtering
     if (queries?.title) formatedQueries.title = {$regex: queries.title, $options: 'i'}
     let queryCommand = Product.find(formatedQueries)
@@ -44,6 +42,20 @@ const getAllProduct = asyncHandler(async(req, res) => {
         const sortBy = req.query.sort.split(',').join(' ')
         queryCommand = queryCommand.sort(sortBy)
     }
+
+    // Fields limiting
+    if (req.query.fields) {
+        const fields = req.query.fields.split(',').join(' ')
+        queryCommand = queryCommand.select(fields)
+
+    }
+
+    // Pagination
+    const page = +req.query.page || 1
+    const limit = +req.query.limit || process.env.LIMIT_PRODUCTS
+    const skip = (page - 1) * limit
+    queryCommand.skip(skip).limit(limit)
+
 
     queryCommand.exec()
         .then(async(response) => {
@@ -85,10 +97,33 @@ const deleteProduct = asyncHandler(async(req, res) => {
     })
 })
 
+const ratings = asyncHandler(async(req, res) => {
+    const {_id} = req.user 
+    const {star, comment, pid} = req.body
+    if (!star || !pid) throw new Error('Missing inputs')
+    const ratingProduct = await Product.findById(pid)
+    const alreadyRating = ratingProduct?.ratings?.find(item => item.postedBy.toString() === _id)
+
+    if (alreadyRating) {
+        // await Product.updateOne({
+            
+        // })
+    } else {
+        const response = await Product.findByIdAndUpdate(pid, {
+            $push: {ratings: {star, comment, postedBy: _id}}
+        }, {new: true})
+    }
+
+    return res.status(200).json({
+        status: true,
+    })
+})
+
 module.exports = {
     createProduct,
     getProduct,
     getAllProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    ratings
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -5,6 +5,7 @@ const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
 router.post('/create', [verifyAccessToken, isAdmin], controllers.createProduct)
 router.get('/infor-product', controllers.getProduct)
 router.get('/infor-all-product', controllers.getAllProduct)
+router.put('/ratings', verifyAccessToken, controllers.ratings)
 router.put('/update/:pid', [verifyAccessToken, isAdmin], controllers.updateProduct)
 router.delete('/:pid', [verifyAccessToken, isAdmin], controllers.deleteProduct)
 
